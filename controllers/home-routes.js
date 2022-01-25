a341f0b505059cf396e712f34178a85c48b9f4f5
Jason Wisniewski <jwizinpa95@gmail.com>
2022-01-25 11:54:47 -0700

    sequelize session added and login/out functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-const res = require('express/lib/response');
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const { Post, User, Comment, Vote } = require('../models');
 
+// get all posts for homepage
 router.get('/', (req, res) => {
+  console.log('======================');
   Post.findAll({
     attributes: [
       'id',
@@ -28,8 +29,8 @@ router.get('/', (req, res) => {
     ]
   })
     .then(dbPostData => {
-      // pass a single post object into the homepage template
-      const posts = dbPostData.map(post => post.get({ plain:true}));
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+
       res.render('homepage', { posts });
     })
     .catch(err => {
@@ -38,4 +39,13 @@ router.get('/', (req, res) => {
     });
 });
 
-module.exports= router;
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
